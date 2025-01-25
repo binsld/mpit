f6752dcf9b5254dfd883e275d34adf6982eb8a64
binsld <binfog7@gmail.com>
2025-01-26 00:00:55 +0300

    Some web and api structure

--- web/main.py
+++ web/main.py
@@ -1,7 +1,26 @@
 from flask import Flask
 
 app = Flask(__name__)
+test_head = "<a href='/'>Главная</a> <a href='/profile'>Профиль</a> <a href='/moderator'>Страничка модератора</a> <a href='/administrator'>Страничка админа</a><br>"
 
 @app.get("/")
-def hello_world():
-    return ("<p>Hello, World!</p>")
+def root():
+    return (test_head+"<p>Здесь у нас находится мерч</p>")
+
+
+@app.get("/logs")
+def logs():
+    return (test_head+"<p>Здесь у нас находятся логи</p>")
+
+@app.get("/profile")
+def profile():
+    return (test_head+"<p>Здесь у нас находится профиль пользователя</p>")
+
+@app.get("/moderator")
+def moderator():
+    return (test_head+"<p>Здесь у нас находится панель управления модератора</p>")
+
+
+@app.get("/administrator")
+def administrator():
+    return (test_head+"<p>Здесь у нас находится панель управления администратора</p><br><a href='/logs'>Логи</a>")
