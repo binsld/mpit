30920c95ff47de72cafdc18ff51b4b71114aa452
binsld <binfog7@gmail.com>
2025-01-27 01:28:22 +0300

    API auth system

--- api/main.py
+++ api/main.py
@@ -1,16 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from urllib.parse import unquote
+from config import TOKEN
+from hashlib import sha256, md5
+from json import loads
+from datetime import datetime
+from random import randrange
+import hmac
 import db
 app = FastAPI()
 
+def dcode_and_verify(data):
+    try:
+        vals = {k: unquote(v) for k, v in [s.split('=', 1) for s in data.split('&')]}
+        identificator = loads(vals["user"])['id']
+        data_check_string = '\n'.join(f"{k}={v}" for k, v in sorted(vals.items()) if k != 'hash')
+        secret = hmac.new("WebAppData".encode(),TOKEN.encode(),sha256)
+        newhash = hmac.new(secret.digest(),data_check_string.encode(),sha256).hexdigest()
+        return vals['hash'] == newhash, identificator
+    except:
+        return False, 0
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def validate(token):
+    auth_data: db.Authtoken | None = db.session.query(db.Authtoken).filter(db.Authtoken.token == token).first()
+    if auth_data is None:
+        return 0,0
+    return auth_data.user_id, auth_data.access
 
-@app.get("/whoami/{identificator}")
-async def whoami(identificator):
-    return db.session.query(db.User).filter(db.User.telegram_id==identificator).first()
-
-@app.post("/new-event")
-async def new_event(username: str, password: str):
-    return print(username, password)
+@app.post("/auth")
+async def auth(request: Request):
+    a = await request.body()
+    answer, identificator = dcode_and_verify(a.decode())
+    if answer:
+        user: db.User | None = db.session.query(db.User).filter(db.User.telegram_id == identificator).first()
+        if user is None:
+            return "Not registered"
+        newtoken = (md5(f"{randrange(10000000000)}.{randrange(10000000000)}".encode()).hexdigest()
+                    +'0'*(10-len(hex(identificator)))+hex(identificator)[2:])
+        db.session.add(db.Authtoken(user_id=identificator, created=datetime.now(), token=newtoken, access=user.type))
+        db.session.commit()
+        return f"{identificator}"
+    return "Data corrupred"
